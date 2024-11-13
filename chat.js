1d6a9cf2243f84d01e276881c1d6456b389b9798
Greg Clinton <greg.clinton@gmail.com>
2024-11-13 11:21:17 -0800

    latest

--- chat.js
+++ chat.js
@@ -1,4 +1,13 @@
 chat = {
+    const peeps = {
+        register: (name, instructions) => {
+            const peep = { name: name, instructions: instructions };
+    
+            peeps[name] = peep;
+            return  peep;
+        }
+    }
+
     messages: [],
     peep: peeps.register('ai', 'You are a helpful assistant.'),
 
--- peeps.js
+++
@@ -1,8 +0,0 @@
-const peeps = {
-    register: (name, instructions) => {
-        const peep = { name: name, instructions: instructions };
-
-        peeps[name] = peep;
-        return  peep;
-    }
-}
